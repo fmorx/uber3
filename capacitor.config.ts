cbe1810b4bcad375f649ec63c03863c321b7efc9
Frankye More ayala <frankyemoreayala@MacBook-Air-de-Frankye.local>
2024-08-28 13:43:38 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uber',
+  webDir: 'www'
+};
+
+export default config;
